92a9c8865beb241aa7672a19abb4a420221d39b9
Cooper Eisman <cooper@eisman.me>
2022-03-20 22:08:17 -0600

    Created Main.java

---
+++ src/main.java
@@ -0,0 +1,10 @@
+// Author:      Cooper Eisman
+// Created:     2207 20MAR22
+// Modified:    2208 20MAR22
+// Purpose:     Main Runner for Wordle Goblin App
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
